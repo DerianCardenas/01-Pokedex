a758dc4123fc51ee60675e23aedde3ec3d9979c1
Derian Cardenas <cardenasmorteraderian@gmail.com>
2024-08-09 11:55:44 -0700

    conexiones a github

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/01-Pokedex/', 
   plugins: [
     vue(),
     vueDevTools(),
